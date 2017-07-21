6beafe581ff0388849e97905fed6ce54d8c7d6a6
Agustín Santiago Gutiérrez <elsantodel90@gmail.com>
2017-07-21 01:41:00 -0300

    Agrego al script soporte para los concursos upsolving (no mira penalidad porque no hay)

--- scoreboardToCsv.py
+++ scoreboardToCsv.py
@@ -5,6 +5,7 @@ import sys
 import re
 
 COJ_URL = "http://coj.uci.cu/contest/cscoreboard.xhtml?cid={}&ungroup"
+COJ_URL_UPSOLVING = "http://coj.uci.cu/tables/cscoreboard3.xhtml?cid={}"
 
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
@@ -12,7 +13,21 @@ if __name__ == "__main__":
 	else:
 		contestId = sys.argv[1]
 		r = requests.get(COJ_URL.format(contestId))
+		nothing = True
 		for position, (team, problems, penalty) in enumerate(re.findall('<tr>.*?class="team".*?title="(8camparg..).*?".*?</td>.*?<td><a.*?>(.*?)</a>.*?<td>(.*?)</td>.*?</tr>', r.text, re.DOTALL)):
+			if nothing:
+				sys.stdout.write(",".join(["Posicion" ,"Equipo", "Problemas", "Penalidad"]))
+				sys.stdout.write("\n")					
+				nothing = False
 			sys.stdout.write(",".join([str(position+1) ,team, problems, penalty]))
 			sys.stdout.write("\n")
+			
+		if nothing:
+			# Assume upsolving
+			r = requests.get(COJ_URL_UPSOLVING.format(contestId))
+			sys.stdout.write(",".join(["Posicion" ,"Equipo", "Problemas"]))
+			sys.stdout.write("\n")					
+			for position, (team, problems) in enumerate(re.findall('<tr class=".*?">.*?<a title="(8camparg..).*?".*?</td>.*?<td>.*?</td>.*?<td>.*?<a.*?>(.*?)</a></td>.*?</tr>', r.text, re.DOTALL)):
+				sys.stdout.write(",".join([str(position+1) ,team, problems]))
+				sys.stdout.write("\n")
 		
